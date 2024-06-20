99ba9e9b5dc695943f7d1ccb15a348b420f6bdf3
Nant1ng <andressantana99@hotmail.se>
2024-06-20 19:16:18 +0200

    Integration test of Api works

---
+++ CarSimulator.Test/Services/DriverServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using SimulatorLibrary.Services;
+
+namespace CarSimulator.Test.Services
+{
+    [TestClass]
+    public class DriverServiceTest
+    {
+        [TestMethod]
+        public async Task Generate_Vaild_Random_Driver()
+        {
+            // Arrange
+            var service = new DriverService();
+
+            // Act
+            var driver = await service.GetRandomDriver();
+
+            // Assert 
+            Assert.IsNotNull(driver);
+            Assert.IsFalse(string.IsNullOrEmpty(driver.Title));
+            Assert.IsFalse(string.IsNullOrEmpty(driver.Name));
+            Assert.IsFalse(string.IsNullOrEmpty(driver.Gender));
+        }
+    }
+}
